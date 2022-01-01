52e92065bd7efdfd3e8220adde9433e331a95095
Parikshit Hooda <phooda804@live.com>
2022-01-02 03:00:49 +0530

    setCount implementation change changed implementation from setCount(count+1) to setCount(count => count+1) safer type.

--- src/HookCounter2.js
+++ src/HookCounter2.js
@@ -4,7 +4,9 @@ function HookCounter2() {
   const [count, setCount] = useState(0);
 
   const changeCount = () => {
-    count % 2 === 0 ? setCount(count + 1) : setCount(count - 1);
+    count % 2 === 0
+      ? setCount((count) => count + 1)
+      : setCount((count) => count - 1);
   };
 
   return (
