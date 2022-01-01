401eda1d125f8fd16baf020bce99197974757467
Parikshit Hooda <phooda804@live.com>
2022-01-02 02:56:13 +0530

    React Hooks Tutorial - 3 - useState with previous state Increment or decrement based on current count

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import HookCounter from "./HookCounter";
+import HookCounter2 from "./HookCounter2";
 
 export default function App() {
   return (
     <div className="App">
       <HookCounter />
+      <HookCounter2 />
     </div>
   );
 }
---
+++ src/HookCounter2.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function HookCounter2() {
+  const [count, setCount] = useState(0);
+
+  const changeCount = () => {
+    count % 2 === 0 ? setCount(count + 1) : setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={changeCount}>Count: {count}</button>
+    </div>
+  );
+}
+
+export default HookCounter2;
