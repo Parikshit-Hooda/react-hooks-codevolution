d859e8e9a0ca02a9bc4efa77346eb454fe7397f8
Parikshit Hooda <phooda804@live.com>
2022-01-02 03:33:43 +0530

    React Hooks Tutorial - 5 - useState with array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./styles.css";
 // import HookCounter from "./HookCounter";
 // import HookCounter2 from "./HookCounter2";
-import HookCounter3 from "./HookCounter3";
+// import HookCounter3 from "./HookCounter3";
+import HookCounter4 from "./HookCounter4";
 
 export default function App() {
   return (
     <div className="App">
       {/* <HookCounter /> */}
       {/* <HookCounter2 /> */}
-      <HookCounter3 />
+      {/* <HookCounter3 /> */}
+      <HookCounter4 />
     </div>
   );
 }
