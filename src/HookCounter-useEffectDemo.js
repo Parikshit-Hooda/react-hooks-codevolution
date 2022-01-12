fbca7d0f9680115528f0e186b08827d438c5a8cb
Parikshit Hooda <phooda804@live.com>
2022-01-13 04:12:31 +0530

    useEffect conditional run demo

--- src/HookCounter-useEffectDemo.js
+++ src/HookCounter-useEffectDemo.js
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 
 function HookCounteruseEffectDemo() {
   const [count, setCount] = useState(0);
+  const [consoleLogCount, setconsoleLogCount] = useState(0);
 
   useEffect(() => {
-    document.title = `clicked ${count} times`;
-  });
+    console.log(`clicked ${count} times`);
+    // document.title = `clicked ${count} times`;
+  }, [consoleLogCount]);
 
   return (
     <div>
       <button onClick={() => setCount(count + 1)}>Clicked {count} times</button>
+      <button onClick={() => setconsoleLogCount(consoleLogCount + 1)}>
+        Clicked {consoleLogCount} times
+      </button>
     </div>
   );
 }
